9030506f68d4ea58a72913c9f471bda57b366715
Andy Koski <akoski83@gmail.com>
2025-12-21 01:14:30 -0500

    fix(prompt): Export PromptFormatter and ContextBundle from index

--- src/prompt/index.ts
+++ src/prompt/index.ts
@@ -2,3 +2,4 @@ export { BudgetAllocator, BudgetValidationError } from './allocator.js';
 export type { ContextItem, ContextCategory, AllocationResult, CategoryBudget } from './allocator.js';
 export { PromptPackGenerator } from './generator.js';
 export type { PromptPack, PromptPackManifest, PromptSection, GeneratorOptions } from './generator.js';
+export type { PromptFormatter, ContextBundle, ContextFile } from './types.js';
